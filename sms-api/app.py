f47f219aad5fbdb323af6fca17286d3dff32562d
Austin M. Edger <ame2194@columbia.edu>
2021-10-31 18:59:49 -0400

    Update app.py

--- sms-api/app.py
+++ sms-api/app.py
@@ -18,18 +18,32 @@ db = firestore.client()
 # Dictionary API base URL
 base_url = 'https://api.dictionaryapi.dev/api/v2/entries/en/'
 
-# TEST
 
-
-@app.route('/')
-def hello():
-    return "hello world"
+@app.route("/", methods=['GET', 'POST'])
+def sms_reply():
+    phone_number = request.values.get('From')
+    word = request.values.get('Body').lower()
+    word_dict = get_word_dict(word)
+    response = make_sms_response(word_dict)
+    pass
 
 
 @app.route('/word/<word>', methods=['GET'])
 def word(word):
     word_dict = get_word_dict(word)
-    return jsonify(get_word_definition(word_dict))
+    return jsonify(word_dict)
+
+
+@app.route('/sms-response/<word>', methods=['GET'])
+def sms_response(word):
+    word_dict = get_word_dict(word)
+    response = make_sms_response(word_dict)
+    return response
+
+
+@app.route("/api/dashboard", methods=['GET'])
+def dashboard():
+    pass
 
 
 def get_word_dict(word):
@@ -37,12 +51,21 @@ def get_word_dict(word):
     return data.json()[0]
 
 
-def get_word_definition(word_dict):
-    return word_dict['meanings'][0]['definitions'][0]['definition']
-
-
 # Will be used make a string of whatever we want to text.
 
 
 def make_sms_response(word_dict):
-    return
+
+    word = word_dict['word']
+    word_definition = word_dict['meanings'][0]['definitions'][0]
+
+    definition = word_definition['definition']
+    example = word_definition['example']
+
+    response = """
+    %s
+    Definition: %s
+    Example: %s
+    """ % (word, definition, example)
+
+    return response
